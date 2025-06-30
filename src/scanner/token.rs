1ac40a5276fdaf299e3307cb364c71f428e79028
Yury Lebedev <y.d.lebedev@gmail.com>
2025-06-29 18:53:21 -0700

    move enum to own create

--- src/scanner/token.rs
+++ src/scanner/token.rs
@@ -5,56 +5,9 @@ use std::{
     str::Chars,
 };
 
-#[derive(Debug)]
-enum TokenType {
-    // Single-character tokens.
-    LeftParen,
-    RightParen,
-    LeftBrace,
-    RightBrace,
-    Comma,
-    Dot,
-    Minus,
-    Plus,
-    Semicolon,
-    Slash,
-    Star,
+use token_type::TokenType;
 
-    // One or two character tokens.
-    Bang,
-    BangEqual,
-    Equal,
-    EqualEqual,
-    Greater,
-    GreaterEqual,
-    Less,
-    LessEqual,
-
-    // Literals.
-    Identifier,
-    String(String),
-    Number(f64), // this can be optimized with different num types
-
-    // Keywords.
-    And,
-    Class,
-    Else,
-    False,
-    Fn,
-    For,
-    If,
-    Nil,
-    Or,
-    Print,
-    Return,
-    Super,
-    This,
-    True,
-    Let,
-    While,
-
-    EOF,
-}
+mod token_type;
 
 #[derive(Debug)]
 pub(super) enum TokenError {
@@ -70,17 +23,6 @@ pub(crate) struct Token {
 }
 
 type I<'a> = Peekable<Enumerate<Chars<'a>>>;
-
-impl Token {
-    pub(super) fn read_next(iter: &mut I) -> Result<Option<Self>, TokenError> {
-        let token = TokenType::from(iter)?;
-
-        Ok(match token {
-            Some(token_type) => Some(Self { token_type }),
-            None => None,
-        })
-    }
-}
 
 impl From<ParseFloatError> for TokenError {
     fn from(value: ParseFloatError) -> Self {
@@ -107,42 +49,51 @@ impl fmt::Display for TokenError {
     }
 }
 
-impl TokenType {
+impl Token {
+    pub(super) fn read_next(iter: &mut I) -> Result<Option<Self>, TokenError> {
+        let token = Token::from(iter)?;
+
+        Ok(match token {
+            Some(token_type) => Some(Self { token_type }),
+            None => None,
+        })
+    }
+
     fn from(iter: &mut I) -> Result<Option<TokenType>, TokenError> {
         let token = match iter.next() {
             Some((ind, c)) => match c {
-                '(' => Some(Self::LeftParen),
-                ')' => Some(Self::RightParen),
-                '{' => Some(Self::LeftBrace),
-                '}' => Some(Self::RightBrace),
-                ',' => Some(Self::Comma),
-                '.' => Some(Self::Dot),
-                '-' => Some(Self::Minus),
-                '+' => Some(Self::Plus),
-                ';' => Some(Self::Semicolon),
-                '*' => Some(Self::Star),
+                '(' => Some(TokenType::LeftParen),
+                ')' => Some(TokenType::RightParen),
+                '{' => Some(TokenType::LeftBrace),
+                '}' => Some(TokenType::RightBrace),
+                ',' => Some(TokenType::Comma),
+                '.' => Some(TokenType::Dot),
+                '-' => Some(TokenType::Minus),
+                '+' => Some(TokenType::Plus),
+                ';' => Some(TokenType::Semicolon),
+                '*' => Some(TokenType::Star),
                 // Potentially compound tokens
-                '!' if TokenType::match_next_token(iter, &'=') => Some(Self::BangEqual),
-                '!' => Some(Self::Bang),
-                '=' if TokenType::match_next_token(iter, &'=') => Some(Self::EqualEqual),
-                '=' => Some(Self::Equal),
-                '<' if TokenType::match_next_token(iter, &'=') => Some(Self::LessEqual),
-                '<' => Some(Self::Less),
-                '>' if TokenType::match_next_token(iter, &'=') => Some(Self::GreaterEqual),
-                '>' => Some(Self::Greater),
+                '!' if Token::match_next_token(iter, &'=') => Some(TokenType::BangEqual),
+                '!' => Some(TokenType::Bang),
+                '=' if Token::match_next_token(iter, &'=') => Some(TokenType::EqualEqual),
+                '=' => Some(TokenType::Equal),
+                '<' if Token::match_next_token(iter, &'=') => Some(TokenType::LessEqual),
+                '<' => Some(TokenType::Less),
+                '>' if Token::match_next_token(iter, &'=') => Some(TokenType::GreaterEqual),
+                '>' => Some(TokenType::Greater),
                 // division or comment
-                '/' if TokenType::match_next_token(iter, &'/') => {
-                    TokenType::consume_comment(iter);
+                '/' if Token::match_next_token(iter, &'/') => {
+                    Token::consume_comment(iter);
                     None
                 }
-                '/' => Some(Self::Slash),
+                '/' => Some(TokenType::Slash),
                 // Strings
-                '"' => Some(Self::String(TokenType::consume_string(iter, ind)?)),
+                '"' => Some(TokenType::String(Token::consume_string(iter, ind)?)),
                 // ignored chars
                 // TODO: Increment on line on new line
                 ' ' | '\r' | '\t' | '\n' => None,
                 // Numbers
-                c if c.is_digit(10) => Some(Self::Number(TokenType::consume_number(iter, c)?)),
+                c if c.is_digit(10) => Some(TokenType::Number(Token::consume_number(iter, c)?)),
                 _ => {
                     return Err(TokenError::InvalidToken(c, ind));
                 }
---
+++ src/scanner/token/token_type.rs
@@ -0,0 +1,50 @@
+#[derive(Debug)]
+pub(super) enum TokenType {
+    // Single-character tokens.
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    // One or two character tokens.
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // Literals.
+    Identifier,
+    String(String),
+    Number(f64), // this can be optimized with different num types
+
+    // Keywords.
+    And,
+    Class,
+    Else,
+    False,
+    Fn,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Let,
+    While,
+
+    EOF,
+}
